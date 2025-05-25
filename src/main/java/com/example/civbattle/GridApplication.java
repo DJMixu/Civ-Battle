7b088016f5e1f033ec97262f5609440d59873c35
mrKubs0n <jakub12kk92@gmail.com>
2025-05-25 19:38:59 +0200

    Cleanup 2.0, usunięto nieużywane rzeczy + wstęp komentarzy do dokumentacji

--- src/main/java/com/example/civbattle/GridApplication.java
+++ src/main/java/com/example/civbattle/GridApplication.java
@@ -230,6 +230,26 @@ public class GridApplication extends Application {
     }
 
     public static void main(String[] args) {
+        Symulacja sim = new Symulacja(60,60,3,"Ala ma kota");
+        sim.krokSymulacji();
+        sim.plansza.wypisz();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.krokSymulacji();
+        sim.plansza.wypisz();
         System.out.println("start");
         launch();
     }
