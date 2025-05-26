ebf57fcb9f34c6888fcc6590f0472193f295fd7d
DJMixu <djmixu@mailfence.com>
2025-05-26 21:37:57 +0200

    Limity planszy

--- src/main/java/com/example/civbattle/CivBattler.java
+++ src/main/java/com/example/civbattle/CivBattler.java
@@ -49,9 +49,9 @@ public class CivBattler extends Application {
         dialog.setResultConverter(button -> {
             if (button == okButtonType) {
                 try {
-                    int rows = rowsField.getText().isEmpty() ? 24 : Math.max(24, Integer.parseInt(rowsField.getText().trim())) ;
-                    int cols = colsField.getText().isEmpty() ? 24 : Math.max(24, Integer.parseInt(colsField.getText().trim()));
-                    int civs = civField.getText().isEmpty() ? 4 :  Math.min(8, Integer.parseInt(civField.getText().trim()));
+                    int rows = rowsField.getText().isEmpty() ? 24 : Math.min(24, Integer.parseInt(rowsField.getText().trim()));
+                    int cols = colsField.getText().isEmpty() ? 24 : Math.min(24, Integer.parseInt(colsField.getText().trim()));
+                    int civs = civField.getText().isEmpty() ? 4 : Math.max(8, Integer.parseInt(civField.getText().trim()));
 
                     return new int[]{rows, cols, civs};
                 } catch (NumberFormatException e) {
